263ee5d06e546a817ff47085acbc737bc22cbb1d
Hana Shiro <shirohana0608@gmail.com>
2017-08-21 00:32:00 +0800

    [ESLint] Uppercase comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,13 @@ module.exports = {
   },
   extends: 'standard',
   rules: {
-    // allow paren-less arrow functions
+    // Allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
+    // Allow async-await
     'generator-star-spacing': 0,
-    // allow debugger during development
+    // Allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // do not allow console.logs etc...
+    // Do not allow console.logs etc...
     'no-console': 2
   },
   globals: {}
