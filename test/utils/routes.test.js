6a234f444965da1fb97ae3d5bcaa1a7918753b07
Hana Shiro <shirohana0608@gmail.com>
2017-08-23 05:01:35 +0800

    [Common] Refactor `utils/routes`

--- test/utils/routes.test.js
+++ test/utils/routes.test.js
@@ -1,38 +1,15 @@
 import test from 'ava'
 import { Utils } from '../../index.js'
 
-test('isFilenameSimple(filename)', t => {
-  const isSimple = Utils.isFilenameSimple
-
-  t.true(isSimple('get.js'))
-  t.true(isSimple('get'))
-  t.false(isSimple('getUser.js'))
-  t.false(isSimple('g0t.js'))
-})
-
-test('isFilenameValid(filename)', t => {
-  const isValid = Utils.isFilenameValid
-
-  t.true(isValid('get.js'))
-  t.true(isValid('get'))
-  t.true(isValid('getUser.js'))
-  t.true(isValid('getUserProfile.js'))
-  t.true(isValid('getUser_name-Profile.js'))
-  t.true(isValid('abC-D_E.js'))
-  t.true(isValid('methodPath1-Path2_Param1.js'))
-  t.true(isValid('method-Path1-Path2_Param1.js'))
-  t.false(isValid('GetUser.js'))
-})
-
 test('parseKeys(filename)', t => {
   const parse = (file) => Utils.parseKeys(file).join('/')
 
   t.is('get', parse('get.js'))
   t.is('user/get', parse('user/get.js'))
   t.is('user/get', parse('getUser.js'))
-  t.is('user/:user_id/get', parse('user/_user-id/get.js'))
-  t.is('user/:user_id/get', parse('getUser_userId.js'))
-  t.is('page/:page_num/get', parse('page/_page-num/get.js'))
+  t.is('user/:user_id/get', parse('user/_user_id/get.js'))
+  t.is('user/:userId/get', parse('getUser_userId.js'))
+  t.is('page/:page-num/get', parse('page/_page-num/get.js'))
   t.is('', parse('_invalid.js'))
 })
 
@@ -50,7 +27,8 @@ test('ensureRoutes()', t => {
 
   Utils.ensureRoutes(root, routeKeys)
   routeKeys.forEach((key) => {
-    t.is(typeof parent.children[0], 'object')
-    parent = parent.children[0]
+    const type = (key.startsWith(':') ? 'paramChildren' : 'children')
+    parent = parent[type][key]
+    t.is(typeof parent, 'object')
   })
 })
