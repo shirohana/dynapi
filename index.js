f7023fb92f59e580ba0c6c9c9c0c67eea1ff9e3a
Hana Shiro <shirohana0608@gmail.com>
2017-08-24 15:07:03 +0800

    [Main] Update `index`

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 require('source-map-support').install()
 
-module.exports = require('./dist/dynapi')
+const dynapi = require('./dist/dynapi')
+
+exports = module.exports = dynapi['factory']
+
+Object.keys(dynapi)
+.filter(key => key !== 'factory')
+.forEach(key => exports[key] = dynapi[key])
