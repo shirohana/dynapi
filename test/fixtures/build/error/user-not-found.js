40f2160b7e675b260ab92a2cd489107e4c94800c
Hana Shiro <shirohana0608@gmail.com>
2017-08-29 10:01:50 +0800

    [Test] Update `build.test`

--- test/fixtures/build/error/user-not-found.js
+++ test/fixtures/build/error/user-not-found.js
@@ -1,9 +1,10 @@
-export default class UserNotFoundError extends Error {
+import UserError from './user-error'
+
+export default class UserNotFoundError extends UserError {
   constructor (message) {
     super(message)
 
     this.name = 'UserNotFound'
-    this.status = 404
     this.code = 'USER_NOT_FOUND'
   }
 }
