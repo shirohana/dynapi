b73d99fd121d430223ed8cdca0e04c7e292720ea
Hana Shiro <shirohana0608@gmail.com>
2018-01-25 16:15:14 +0800

    [Test] Update `compatibility.test`

--- test/fixtures/compatibility/server-connect.js
+++ test/fixtures/compatibility/server-connect.js
@@ -1,19 +1,17 @@
 const connect = require('connect')
 const dynapi = require('../../../index')
 
-const options = {
-  dev: true,
-  watch: false,
-  router: {
-    srcdir: 'test/fixtures/compatibility',
-    routesdir: 'api'
-  }
-}
-
 module.exports = function createServer () {
   const app = connect()
 
-  app.use('/api', dynapi(options))
+  app.use(dynapi({
+    dev: false,
+    watch: false,
+    router: {
+      srcdir: 'test/fixtures/compatibility',
+      routesdir: 'routes'
+    }
+  }))
 
   return new Promise(resolve => {
     setTimeout(() => resolve(app), 2000)
